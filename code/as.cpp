93451c2af99caeff92e97d140cef01292ea22441
12xsunx12 <regan_sun12@outlook.com>
2024-08-21 20:40:54 -0500

    init. commit

---
+++ code/as.cpp
@@ -0,0 +1,15 @@
+/***************************************************************
+  Student Name:
+  File Name:
+  Assignment number
+
+  Other comments regarding the file - description of why it is there, etc.
+***************************************************************/
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!";
+
+  return 0;
+}
